c8c736851a3cc493d220b611a9777923cc1e8aa5
RastBerryHI <musaev.pasha@gmail.com>
2021-02-18 15:53:35 +0200

    Commit by Pasha
    Баззовый и первый коммит, работай с ветками, создай свою ветку

---
+++ ClearApp/ClearApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Forms;
+using ClearApp.Views;
+
+
+namespace ClearApp
+{
+   
+    public partial class App : Application
+    {
+        
+
+        public App()
+        {
+            InitializeComponent();
+
+            // Страница отображающаяся при открытии приложения
+            MainPage = new Buttons();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ClearApp/ClearApp/AudioService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Android.Media;
+using Android.Content.Res;
+using ClearApp;
+
+[assembly: Dependency(typeof(AudioService))]
+namespace ClearApp
+{
+    class AudioService  
+    {
+        public AudioService()
+        {
+        }
+
+        public void PlayAudioFile(string fileName)
+        {
+            var player = new MediaPlayer();
+            var fd = global::Android.App.Application.Context.Assets.OpenFd(fileName);
+            player.Prepared += (s, e) =>
+            {
+                player.Start();
+            };
+            player.SetDataSource(fd.FileDescriptor, fd.StartOffset, fd.Length);
+            player.Prepare();
+        }
+    }
+}
